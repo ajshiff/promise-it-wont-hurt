97f035283552751f990b2d06deb755a45825ab4c
Aaron Shiffler <ajshiffler@gmail.com>
2018-09-07 18:29:42 -0600

    Solved e02 in two ways

---
+++ pws_e02.js
@@ -0,0 +1,13 @@
+// My thought
+var promise1 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 300, 'FULFILLED!');
+});
+promise1.then(console.log);
+
+// Ben's Thought
+function promise2 () {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, 300, 'FULFILLED!');
+    });
+}
+promise2.then(console.log);
