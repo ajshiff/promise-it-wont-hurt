474ff6f25dd4687deb21b205dae2e4e06be8e483
Aaron Shiffler <ajshiffler@gmail.com>
2018-09-10 13:10:20 -0600

    completed e07. Note that first() and second() are defined by the exercise and don't need to be created

--- pws_e07.js
+++ pws_e07.js
@@ -1,14 +1,12 @@
-console.log(process.argv[2]);
+// var first  = (secretValue) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(resolve, 250, secretValue);
+//     });
+// };
+// var second = (value) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout (resolve, 250, value);
+//     });
+// };
 
-var first  = (secretValue) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, 250, secretValue);
-    });
-};
-var second = (value) => {
-    return new Promise((resolve, reject) => {
-        setTimeout (resolve, 250, value);
-    });
-};
-
-first('secret').then(second).then(console.log);
+first().then(second).then(console.log);
